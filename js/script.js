4889d4d24e6c2c3c032e696e74127866ec6a3705
Donovan <d_mcrae@ymail.com>
2016-04-13 17:02:39 -0400

    adding styles from w02/d03 workshop

--- js/script.js
+++ js/script.js
@@ -13,6 +13,34 @@ $(document).ready(function(){
 		$('#sec').text(sec);
 		$('#ms').text(ms);
 
+// sets time to white if min is even
+
+		if( (min % 2) == 0){
+			$('h3').css("color" , "white");
+		}else{
+			$('h3').css("color" , "limegreen");
+		}
+
+
+
 	}, 1);
+
+// randomly changes background every 5 mins
+
+		var color = ["red" , "blue" , "purple" , "green" , "orange" , "brown"];
+
+	var bodyChange = setInterval(function(){
+		var rand = Math.floor(Math.random() * color.length);
+
+		$('body').css({background:color[rand]}) 
+
+	}, 300000);
+
+	var titleChange = setInterval(function(){
+		var rand = Math.floor(Math.random() * color.length);
+
+		$('h1').css({color:color[rand]})
+	
+	}, 36000000)
 
 });
